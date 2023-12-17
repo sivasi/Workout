80e8f93a98f85d86a798e4ca4c681b112fa4d624
aditya <aditya.kumar.mec20@itbhu.ac.in>
2023-12-17 11:16:53 +0530

    2.1

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next()
 })
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://luminous-puffpuff-e174e2.netlify.app"
 };
 
 app.use(cors(corsOptions));
